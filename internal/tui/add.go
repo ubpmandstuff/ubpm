eff19cbbeb03fdbc268de5eb131ef34ad08dac70
dura5ka <dura5ka@disroot.org>
2026-01-16 21:08:58 +0300

    tui: implement add view
    this commit implements the entirety of the add view.
    
    Signed-off-by: dura5ka <dura5ka@disroot.org>

--- internal/tui/add.go
+++ internal/tui/add.go
@@ -1,33 +1,48 @@
 package tui
 
 import (
+	"strings"
+
 	"github.com/charmbracelet/bubbles/key"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/huh"
+	"github.com/charmbracelet/lipgloss"
 )
 
 // addKeymap is a struct defining the keys used in the add view
 type addKeymap struct {
 	Help key.Binding
+	Back key.Binding
 	Quit key.Binding
 }
 
+type cAddKeymap struct {
+	k addKeymap // k means keys
+	f *huh.Form // f means form
+}
+
 // ShortHelp returns the keys to show in compact help view
-func (k addKeymap) ShortHelp() []key.Binding {
-	return []key.Binding{k.Help, k.Quit}
+func (c cAddKeymap) ShortHelp() []key.Binding {
+	binds := c.f.KeyBinds()
+	return append(binds, c.k.Help, c.k.Back)
 }
 
 // FullHelp returns the keys to show in complete help view
-func (k addKeymap) FullHelp() [][]key.Binding {
+func (c cAddKeymap) FullHelp() [][]key.Binding {
 	return [][]key.Binding{
-		{k.Help, k.Quit},
+		c.f.KeyBinds(),
+		{c.k.Help, c.k.Back, c.k.Quit},
 	}
 }
 
 var addKeys = addKeymap{
 	Help: key.NewBinding(
-		key.WithKeys("?"),
-		key.WithHelp("?", "show keybindings"),
+		key.WithKeys("f1"),
+		key.WithHelp("f1", "show keybindings"),
+	),
+	Back: key.NewBinding(
+		key.WithKeys("esc"),
+		key.WithHelp("esc", "back to entry list"),
 	),
 	Quit: key.NewBinding(
 		key.WithKeys("q", "ctrl+c"),
@@ -38,29 +53,86 @@ var addKeys = addKeymap{
 type addState struct {
 	keys addKeymap
 	form *huh.Form
+	val  *values
+}
+
+type values struct {
+	title    string
+	username string
+	password string
+	notes    string
 }
 
 func initAddState() addState {
-	return addState{
+	s := addState{
 		keys: addKeys,
-		form: huh.NewForm(),
+		val:  &values{},
 	}
+
+	s.form = huh.NewForm(
+		huh.NewGroup(
+			huh.NewInput().
+				Title("title").
+				Value(&s.val.title),
+			huh.NewInput().
+				Title("username").
+				Value(&s.val.username),
+			huh.NewInput().
+				Title("password").
+				EchoMode(huh.EchoModePassword).
+				Value(&s.val.password),
+			huh.NewText().
+				Title("notes").
+				Lines(3).
+				Value(&s.val.notes),
+		),
+	).WithShowHelp(false)
+
+	return s
 }
 
 func (m model) addUpdate(msg tea.Msg) (tea.Model, tea.Cmd) {
+	form, cmd := m.state.add.form.Update(msg)
+	if f, ok := form.(*huh.Form); ok {
+		m.state.add.form = f
+	}
+
+	if m.state.add.form.State == huh.StateCompleted {
+		v := m.state.add.val
+		m.vault.AddEntry(v.title, v.username, v.password, v.notes)
+		return m, m.switchList()
+	}
+
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch {
 		case key.Matches(msg, m.state.add.keys.Help):
 			m.help.ShowAll = !m.help.ShowAll
+		case key.Matches(msg, m.state.add.keys.Back):
+			return m, m.switchList()
 		case key.Matches(msg, m.state.add.keys.Quit):
 			return m, tea.Quit
 		}
 	}
-	return m, nil
+
+	return m, cmd
 }
 
 func (m model) addView() string {
-	out := ""
+	b1Style := lipgloss.NewStyle().Padding(1, 2)
+
+	var b1 strings.Builder
+
+	b1.WriteString("adding entry\n\n")
+	b1.WriteString(m.state.add.form.View() + "\n\n")
+
+	combined := cAddKeymap{
+		k: m.state.add.keys,
+		f: m.state.add.form,
+	}
+	b1.WriteString(m.help.View(combined))
+
+	out := b1Style.Render(b1.String())
+
 	return out
 }
