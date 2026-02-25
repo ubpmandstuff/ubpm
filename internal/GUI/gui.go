05b790a5b5dee74fc592f07c28cecfa180d56a58
Kikuoku <samsj5j534@gmail.com>
2026-02-25 08:32:26 +0300

    [DOESN'T WORK, DON'T USE IT] temporary changes. Will change and fix it as soon as possible(scream of soul: what did I take that i was THAT high, writing THAT)

--- internal/GUI/gui.go
+++ internal/GUI/gui.go
@@ -1,11 +1,12 @@
 package gui
 
 import (
-	"fmt"
+	"ubpm/internal/vault"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container" // you'll need that @Kikuoku
+	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/widget"
 )
 
@@ -19,8 +20,12 @@ func MakeWindow() {
 	passwordEntry.SetPlaceHolder("Master Password")
 
 	loginButton := widget.NewButton("Open and Unlock Vault", func() {
-		fmt.Println("Placeholder for unlocking logic.") // Заглушка
-		showMainManagerScreen(myWindow)                 // Заглушка
+		v, err := vault.Open(pathEntry.Text, []byte(passwordEntry.Text))
+		if err != nil {
+			dialog.ShowError(err, myWindow)
+			return
+		}
+		showMainManagerScreen(myWindow)
 	})
 
 	loginScreen := container.NewVBox(
@@ -34,20 +39,39 @@ func MakeWindow() {
 	myWindow.ShowAndRun()
 }
 
-func showMainManagerScreen(myWindow fyne.Window) { // Заглушка
+func showMainManagerScreen(myWindow fyne.Window) {
 
 	listLabel := widget.NewLabel("Welcome! Entries will appear here once loaded.")
 
 	addButton := widget.NewButton("Add New Entry", func() {
-		fmt.Println("Add entry UI logic needed.")
+		titleEntry := widget.NewEntry()
+		userEntry := widget.NewEntry()
+		passEntry := widget.NewPasswordEntry()
+		notesEntry := widget.NewMultiLineEntry()
+		formItem := []*widget.FormItem{
+			{Text: "Title", Widget: titleEntry},
+			{Text: "Username", Widget: userEntry},
+			{Text: "Password", Widget: passEntry},
+			{Text: "Notes", Widget: notesEntry},
+		}
+	})
+	dialog.ShowForm("Add New Password", "Add", "Cancel", formItem, func(confirm bool) {
+		if !confirm {
+			return
+		}
+		err := v.AddEntry(
+			titleEntry.Text,
+			userEntry.Text,
+			passEntry.Text,
+			notesEntry.Text,
+		)
+
+		if err != nil {
+			dialog.ShowError(err, myWindow)
+		}
 	})
 
-	// saveButton := widget.NewButton("Save All Changes", func() {
-	// 	fmt.Println("Save logic needed.")
-	// })
-
 	mainContent := container.NewBorder(
-		// container.NewHBox(widget.NewLabel("Password Entries:"), layout.NewSpacer(), saveButton),
 		container.NewHBox(widget.NewLabel("Password Entries:")),
 		addButton,
 		nil,
